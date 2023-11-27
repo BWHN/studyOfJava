06b70eeb9c0f746edc12cc4fb38025c5b238ffa7
hunan <1369165806@qq.com>
2023-11-27 22:49:25 +0800

    超时模拟代码修改

--- solution/src/main/java/org/study/java/solution/interface_retry/MayFailureInterface.java
+++ solution/src/main/java/org/study/java/solution/interface_retry/MayFailureInterface.java
@@ -1,13 +1,21 @@
 package org.study.java.solution.interface_retry;
 
-import java.util.concurrent.TimeoutException;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
 public class MayFailureInterface {
     private int i = 0;
-    public void doSomething() throws TimeoutException {
-        if (i++ < 2) {  // 模拟失败
-            throw new TimeoutException();
+    public void doSomething() {
+        if (i++ < 2) {  // 模拟超时
+            try {
+                log.info("receive request");
+                TimeUnit.SECONDS.sleep(600);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
-        System.out.println("invoke success");
+        log.info("invoke success");
     }
 }
--- solution/src/main/java/org/study/java/solution/interface_retry/RetryTest.java
+++ solution/src/main/java/org/study/java/solution/interface_retry/RetryTest.java
@@ -1,48 +1,65 @@
 package org.study.java.solution.interface_retry;
 
-import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.conn.ConnectTimeoutException;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultHttpRequestRetryHandler;
 import org.apache.http.impl.client.HttpClients;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.net.URI;
+import java.io.IOException;
+import java.net.SocketTimeoutException;
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
-@RestController("/retry")
+@RestController
+@RequestMapping("/retry")
 public class RetryTest {
 
     private static final int RETRY_TIMES = 3;
 
-    @GetMapping("/retryByLoop")
-    public void retryByLoop() throws InterruptedException {
-        MayFailureInterface mayFailureInterface = new MayFailureInterface();
-        int i = 0;
-        while (i++ < RETRY_TIMES) {
-            try {
-                mayFailureInterface.doSomething();
-                break;
-            } catch (TimeoutException e) {
-                System.out.println("timeout, retry times: " + i);
-                Thread.sleep(1000); // 延迟1秒后重试
-            }
-        }
+    /**
+     * 模拟超时接口
+     */
+    @GetMapping("/mayFailure")
+    public void mayFailure() {
+        new MayFailureInterface().doSomething();
     }
 
+    /**
+     * 循环重试
+     */
     @GetMapping("/retryByLoop")
+    public void retryByLoop() throws InterruptedException {
+//        MayFailureInterface mayFailureInterface = new MayFailureInterface();
+//        int i = 0;
+//        while (i++ < RETRY_TIMES) {
+//            try {
+//                mayFailureInterface.doSomething();
+//                break;
+//            } catch (TimeoutException e) {
+//                System.out.println("timeout, retry times: " + i);
+//                TimeUnit.SECONDS.sleep(1); // 延迟1秒后重试
+//            }
+//        }
+    }
+
+    @GetMapping("/retryByClient")
     public void retryByClient() {
         CloseableHttpClient httpClient = HttpClients.custom()
-                .setRetryHandler((response, executionCount, context) -> {
-                    if (executionCount > RETRY_TIMES) { // 超过重试次数，则放弃重试
-                        return false;
-                    }
-                    Throwable cause = response.getCause();
-                    if (cause instanceof TimeoutException) {  // 遇到超时进行重试
-                        return true;
-                    }
-                    return false; // 其他情况不进行重试
-                })
+                .setRetryHandler(new DefaultHttpRequestRetryHandler(RETRY_TIMES, true))
+                .setConnectionTimeToLive(3, TimeUnit.SECONDS)
                 .build();
+        try {
+            HttpGet httpGet = new HttpGet("http://127.0.0.1:8080/retry/mayFailure");
+            httpClient.execute(httpGet);
+        }catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
