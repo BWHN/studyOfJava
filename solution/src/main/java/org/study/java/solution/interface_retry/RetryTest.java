d1a212cd741692a216f01f7067675be83d93822c
zeroxes <1369165806@qq.com>
2023-11-27 20:30:19 +0800

    fix：修改接口重试测试代码调用方式

--- solution/src/main/java/org/study/java/solution/interface_retry/RetryTest.java
+++ solution/src/main/java/org/study/java/solution/interface_retry/RetryTest.java
@@ -1,13 +1,36 @@
 package org.study.java.solution.interface_retry;
 
+import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
 import java.util.concurrent.TimeoutException;
 
-public class RetryByHttpClient {
+@RestController("/retry")
+public class RetryTest {
+
     private static final int RETRY_TIMES = 3;
-    public static void main(String[] args) {
+
+    @GetMapping("/retryByLoop")
+    public void retryByLoop() throws InterruptedException {
+        MayFailureInterface mayFailureInterface = new MayFailureInterface();
+        int i = 0;
+        while (i++ < RETRY_TIMES) {
+            try {
+                mayFailureInterface.doSomething();
+                break;
+            } catch (TimeoutException e) {
+                System.out.println("timeout, retry times: " + i);
+                Thread.sleep(1000); // 延迟1秒后重试
+            }
+        }
+    }
+
+    @GetMapping("/retryByLoop")
+    public void retryByClient() {
         CloseableHttpClient httpClient = HttpClients.custom()
                 .setRetryHandler((response, executionCount, context) -> {
                     if (executionCount > RETRY_TIMES) { // 超过重试次数，则放弃重试
